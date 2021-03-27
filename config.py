a2025f4822f2b26bb9c9a4609a86c0ba413384ad
Vipin Sharma(vipin.sharma) <vipin.sharma@halla.com>
2021-03-27 19:11:39 +0530

    Changed decay rate as per the subset of kitti data being used

--- config.py
+++ config.py
@@ -62,7 +62,7 @@ class NetworkParameters:
     negative_iou_threshold = 0.3
     batch_size = 4
     total_training_epochs = 160
-    iters_to_decay = 101040.    # 15 * 4 * ceil(6733. / 4) --> every 15 epochs on 6733 kitti samples, cf. pillar paper
+    iters_to_decay = 66780 # 101040.    # 15 * 4 * ceil(6733. / 4) --> every 15 epochs on 6733 kitti samples, cf. pillar paper
     learning_rate = 2e-4
     decay_rate = 1e-8
     L1 = 0
