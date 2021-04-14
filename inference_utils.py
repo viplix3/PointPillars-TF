b88d81ed4978dc80db31e1ffa02702e4ee43b1a9
Vipin Sharma <vipin.sharma2024@gmail.com>
2021-04-14 10:51:28 +0530

    yaw and alpha angle changes

--- inference_utils.py
+++ inference_utils.py
@@ -65,13 +65,12 @@ class BBox(Parameters, tuple):
         # z-axis in LiDAR coordinate frame == -(y-axis) of camera coordinate frame
         self.yaw = - self.yaw - np.pi/2
 
-        # if(int(self.heading) == 0):
-        #    self.yaw = - self.yaw
+        if(int(self.heading) == 0) and (self.yaw < 0):
+           self.yaw = - self.yaw
 
         bbox_2d_image_coordinate, bbox_3d_image_coordinate = self.get_2D_BBox(P2) # [num_boxes, box_attributes]
 
         # TODO: Check alpha calculation
-        # alpha = -1.0
         beta = np.arctan2(self.z, self.x)
         alpha = -np.sign(beta) * np.pi/2 + beta + self.yaw
 
@@ -88,7 +87,11 @@ class BBox(Parameters, tuple):
             3. Multiply with LiDAR to camera projection matrix
             4. Multiply with camera to image projection matrix
         """
-        yaw_rotation_matrix = BBox.get_y_axis_rotation_matrix(self.yaw) # rotation matrix around y-axis
+        rotation_angle = self.yaw
+        if int(self.heading) == 0:
+            rotation_angle = -rotation_angle
+
+        yaw_rotation_matrix = BBox.get_y_axis_rotation_matrix(rotation_angle) # rotation matrix around y-axis
         l = self.length
         w = self.width
         h = self.height
